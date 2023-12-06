256be96a7aa414b44fc36d7d9a582fe339422657
Karim Ait-Allaoua <kaitallaoua@gmail.com>
2023-12-05 18:27:56 -0600

    final working driver module and example code

---
+++ code/demo_drive_motors.py
@@ -0,0 +1,20 @@
+from nucleo_driver import MotorDriver
+import time
+motor_driver = MotorDriver()
+
+try:
+
+    while True:
+        motor_driver.control_motors(fl=20, fr=20, bl=20, br=20)
+
+        time.sleep(1)
+        motor_driver.control_motors(fl=-20, fr=-20, bl=-20, br=-20)
+
+        time.sleep(1)
+
+
+except KeyboardInterrupt:
+    print("Exiting Program")
+
+finally:
+    motor_driver.close()
--- code/nucleo_driver.py
+++ code/nucleo_driver.py
@@ -4,96 +4,70 @@ import serial
 
 # https://jetsonhacks.com/2019/10/10/jetson-nano-uart/
 # https://github.com/JetsonHacksNano/UARTDemo/blob/master/uart_example.py
-SERIAL_READ_TIMEOUT = 1
-
-serial_port = serial.Serial(
-    port="/dev/ttyTHS1",
-    baudrate=115200,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    timeout=SERIAL_READ_TIMEOUT # wait at most one second for a response from nucleo
-)
-# Wait a second to let the port initialize
-time.sleep(1)
-
-speed_fast = 70
-speed_slow = 30
-
-# inputs are in the range [-127, 127] where positive numbers drive a motor forward, negative backwards
-def control_motors(fl: int, fr: int, bl: int, br: int):
-    assert fl <= 127 and fl >= -127
-    assert fr <= 127 and fr >= -127
-    assert bl <= 127 and bl >= -127
-    assert br <= 127 and br >= -127
+SERIAL_READ_TIMEOUT = 1  # seconds
 
 
-
-    # drive motor control packet between [129, 144]
-
-    if bl > 0:
-        bl_bit = 1
-    else:
-        bl_bit = 0
-
-    if br > 0:
-        br_bit = 1
-    else:
-        br_bit = 0
-
-    if fl > 0:
-        fl_bit = 1
-    else:
-        fl_bit = 0
-
-    if fr > 0:
-        fr_bit = 1
-    else:
-        fr_bit = 0
-
-    
-
-    motor_dir_control_packet = 129 + ((bl_bit << 3) | (br_bit << 2) | (fl_bit << 1) | (fr_bit << 0))
-
-    print(motor_dir_control_packet)
-    serial_port.write(motor_dir_control_packet.to_bytes(1, 'big'))
-    # any slower and motor speed commands are not reliable
-    time.sleep(0.001)
-
-    serial_port.write(abs(bl).to_bytes(1, 'big'))
-    time.sleep(0.001)
-
-    serial_port.write(abs(br).to_bytes(1, 'big'))
-    time.sleep(0.001)
-
-    serial_port.write(abs(fl).to_bytes(1, 'big'))
-    time.sleep(0.001)
-
-    serial_port.write(abs(fr).to_bytes(1, 'big'))
-    time.sleep(0.001)
-
-try:
-
-    # TODO: add a new control block that the jetson says to unlock/recover from locked motors
-
-    
-    while True:
-        #control_motors(20, 20, 20, 20)
-
-        time.sleep(1)
-        control_motors(fl=-20, fr=-20, bl=20, br=20)
-
-
+class MotorDriver:
+    def __init__(self) -> None:
+        self.serial_port = serial.Serial(
+            port="/dev/ttyTHS1",
+            baudrate=115200,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=SERIAL_READ_TIMEOUT,  # wait at most one second for a response from nucleo
+        )
+        # Wait a second to let the port initialize
         time.sleep(1)
 
+    # inputs are in the range [-127, 127] where positive numbers drive a motor forward, negative backwards
+    def control_motors(self, fl: int, fr: int, bl: int, br: int):
+        assert fl <= 127 and fl >= -127
+        assert fr <= 127 and fr >= -127
+        assert bl <= 127 and bl >= -127
+        assert br <= 127 and br >= -127
 
-except KeyboardInterrupt:
-    print("Exiting Program")
+        # drive motor control packet between [129, 144]
 
-except Exception as exception_error:
-    print("Error occurred. Exiting Program")
-    print("Error: " + str(exception_error))
+        if bl > 0:
+            bl_bit = 1
+        else:
+            bl_bit = 0
 
-finally:
-    serial_port.close()
-    pass
+        if br > 0:
+            br_bit = 1
+        else:
+            br_bit = 0
+
+        if fl > 0:
+            fl_bit = 1
+        else:
+            fl_bit = 0
+
+        if fr > 0:
+            fr_bit = 1
+        else:
+            fr_bit = 0
+
+        motor_dir_control_packet = 129 + (
+            (bl_bit << 3) | (br_bit << 2) | (fl_bit << 1) | (fr_bit << 0)
+        )
+
+        self.serial_port.write(motor_dir_control_packet.to_bytes(1, "big"))
+        # any slower and motor speed commands are not reliable
+        time.sleep(0.001)
+
+        self.serial_port.write(abs(bl).to_bytes(1, "big"))
+        time.sleep(0.001)
+
+        self.serial_port.write(abs(br).to_bytes(1, "big"))
+        time.sleep(0.001)
+
+        self.serial_port.write(abs(fl).to_bytes(1, "big"))
+        time.sleep(0.001)
+
+        self.serial_port.write(abs(fr).to_bytes(1, "big"))
+        time.sleep(0.001)
+
+    def close(self):
+        self.serial_port.close()
